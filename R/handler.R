80c0c84df8bb301e8d534a5290d6b619673d0401
carlosdemoura <lcbarrosdemoura@gmail.com>
2025-02-25 19:17:55 -0300

    changes after first git mess

--- R/handler.R
+++ R/handler.R
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 get_inmet_data = function(year, first.day = NA, last.day = NA, vars = NULL, stations = NULL, local.path = local_data()) {
   dplyr::`%>%`
-=======
-
-get_inmet_raw_data_year = function(year, first.day = NA, last.day = NA, vars = NULL, stations = NULL, local.path = NULL) {
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
 
   csv.lines = validate_dates(year, first.day, last.day) %>%
     {adjust_lines_csv(year, .[[1]], .[[2]])}
 
-<<<<<<< HEAD
   main_dir = file.path(tempdir(), paste0("meteobr_", year))
 
   "https://portal.inmet.gov.br/uploads/dadoshistoricos/" |>
@@ -21,30 +15,6 @@ get_inmet_raw_data_year = function(year, first.day = NA, last.day = NA, vars = N
   dir.create(extract_dir)
   unzip(main_dir, exdir = extract_dir)
   unlink(main_dir, recursive = T)
-=======
-  # days = validate_dates(year, first.day, last.day)
-  # first.day = days[[1]]
-  # last.day  = days[[2]]
-  # csv.lines = adjust_lines_csv(days)
-
-  stations = drop_stations(stations)
-
-  if (is.null(local.path)) {
-    main_dir = file.path(tempdir(), paste0("inmet", year))
-
-    "https://portal.inmet.gov.br/uploads/dadoshistoricos/" |>
-      paste0(year, ".zip") |>
-      download.file(main_dir)
-  } else {
-    main_dir = local.path
-  }
-
-  extract_dir = file.path(tempdir(), "data")
-
-  if (!dir.exists(extract_dir)) dir.create(extract_dir)
-
-  unzip(main_dir, exdir = extract_dir)
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
 
   if (year < 2020) {
     files = list.files(file.path(extract_dir, year), full.names = TRUE)
@@ -52,16 +22,9 @@ get_inmet_raw_data_year = function(year, first.day = NA, last.day = NA, vars = N
     files = list.files(extract_dir, full.names = TRUE)
   }
 
-<<<<<<< HEAD
   full_data = data.frame()
 
   for ( file in files ) {
-=======
-  dados_tratados = data.frame()
-
-  for ( file in files ) {
-
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
     station = file |>
       strsplit("_") |>
       purrr::pluck(1) |>
@@ -93,7 +56,6 @@ get_inmet_raw_data_year = function(year, first.day = NA, last.day = NA, vars = N
       ) %>%
       dplyr::relocate(c(station, time))
 
-<<<<<<< HEAD
     full_data = rbind(full_data, data)
 
   }
@@ -117,40 +79,11 @@ set_data_locally = function(years = 2000:2024) {
 
 
 get_data = function(first.day, last.day, vars = NULL, stations = NULL) {
-=======
-    dados_tratados = rbind(dados_tratados, data)
-  }
-
-  unlink(main_dir)
-  unlink(extract_dir)
-
-  dados_tratados
-}
-
-
-get_github_raw_data_year = function(year, first.day, last.day, vars, stations) {
-
-  int = validate_dates(year, first.day, last.day) %>%
-    {lubridate::interval(lubridate::ymd(.[[1]]), lubridate::ymd(.[[2]]))}
-
-  stations = drop_stations(stations)
-
-  data = year %>%
-    {paste0("C:/Users/Carlos/Downloads/anos/br_", ., ".fst")} %>%
-    fst::fst() %>%
-    as_tibble() %>%
-    filter( lubridate::ymd_h(time) %within% int )
-}
-
-
-get_data = function(first.day, last.day, vars = NULL, stations = NULL, type, source) {
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
   years = fiat_years(first.day, last.day)
 
   full_data = data.frame()
 
   for (year in as.integer(names(years))) {
-<<<<<<< HEAD
     file.path = paste0(local_data(), "/", year, ".Rdata")
 
     if(
@@ -174,57 +107,9 @@ get_data = function(first.day, last.day, vars = NULL, stations = NULL, type, sou
       { if (!is.null(vars))
         dplyr::select(., all_of(c("time", vars)))
         else . }
-=======
-    first.day = years[[as.character(year)]]$first.day
-    last.day = years[[as.character(year)]]$last.day
-
-    if (type == "inmet") {
-      data = get_inmet_raw_data_year(year, first.day, last.day, vars, stations)
-    } else if (type == "github") {
-      data = get_github_raw_data_year(year, first.day, last.day, vars, stations)
-    }
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
 
     full_data = rbind(full_data, data)
   }
 
   full_data
 }
-<<<<<<< HEAD
-=======
-
-
-set_data_locally = function(year, font = "github", path = NULL) {
-  stopifnot("font must be either 'github' or 'inmet'" =
-              font %in% c("github", "inmet"))
-
-  if (is.null(path)) {
-    path = tools::R_user_dir("meteobr", which = "data") |>
-      {\(.) gsub("\\\\", "/", .)}()
-  }
-
-  file = year |>
-    paste0(ifelse(font == "github", ".fst", ".zip"))
-
-  if ( file.exists(file.path(path, file)) ) {
-    if ( tools::md5sum(file.path(path, file)) == verify_hash(file) ) {
-      return("file already available locally")
-    } else {
-      stop(paste0("a different file with the same name is where the local file should be, you must manually clean ", file.path(path, file)))
-    }
-  }
-
-  if (font == "inmet") {
-    "https://portal.inmet.gov.br/uploads/dadoshistoricos/" |>
-      paste0(file) |>
-      download.file(file.path(path, file))
-  } else if (font == "github") {
-    "https://github.com/carlosdemoura/meteobr/tree/master/data/" |>
-      paste0(file) |>
-      download.file(file.path(path, file))
-
-  }
-
-
-}
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
--- R/utils.R
+++ R/utils.R
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-=======
-
-#shas local files
-
-fiat_time_pattern = function(every.time) {
-  build_regex = function(values, digits) {
-    if (is.null(values)) return(stringr::str_c("\\d{", digits, "}"))
-    values = values |>
-      as.character() |>
-      stringr::str_pad(digits, pad = "0")
-    return(stringr::str_c("(", stringr::str_c(values, collapse = "|"), ")"))
-  }
-
-  stringr::str_c(
-    build_regex(every.time$year,  4), "/",
-    build_regex(every.time$month, 2), "/",
-    build_regex(every.time$mday,  2), " ",
-    build_regex(every.time$hour,  2)
-  )
-}
-
-
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
 adjust_lines_csv = function(year, first.day, last.day) {
   between.days = c(first.day, last.day)
 
@@ -58,7 +34,6 @@ adjust_lines_csv = function(year, first.day, last.day) {
 }
 
 
-<<<<<<< HEAD
 validate_dates = function(year = NULL, first.day, last.day) {
   dplyr::`%>%`
 
@@ -82,61 +57,12 @@ validate_dates = function(year = NULL, first.day, last.day) {
                c(first.day, last.day) %>%
                {lubridate::ymd(.[2]) - lubridate::ymd(.[1])} %>%
                {as.integer(.) > 0 })
-=======
-drop_stations = function(x) {
-  x
-}
-
-
-validate_dates = function(year = NULL, first.day, last.day) {
-  pattern_w_year = "\\d{4}[-/]\\d{2}[-/]\\d{2}"
-  pattern_wo_year = "\\d{2}[-/]\\d{2}"
-
-  stopifnot( "days must both include/exclude year" =
-               all(
-                 grepl(pattern_w_year,  first.day) == grepl(pattern_w_year,  last.day),
-                 grepl(pattern_wo_year, first.day) == grepl(pattern_wo_year, last.day)
-                 )
-             )
-
-  stopifnot( "days must in the format yyyy/mm/dd or mm/dd or pass just the year" =
-               (
-                 any(
-                   grepl(pattern_w_year,  first.day),
-                   grepl(pattern_wo_year, first.day)
-                   )
-                 | (
-                   !is.null(year) & all(is.na(c(first.day, last.day)))
-                   )
-               )
-             )
-
-  if (grepl(pattern_w_year, first.day)) {
-    stopifnot( "all days must be in the same year (and equal to the specified year)" =
-                 all(
-                   lubridate::year(first.day) == lubridate::year(last.day),
-                   {if (!is.null(year)) lubridate::year(first.day) == year else T}
-                 )
-               )
-  } else if (grepl(pattern_wo_year, first.day)) {
-    first.day = paste0(year, "/", first.day)
-    last.day  = paste0(year, "/", last.day)
-  } else if (!is.null(year)) {
-    first.day = paste0(year, "/01/01")
-    last.day  = paste0(year, "/12/31")
-
-  }
-
-  stopifnot( "last.day must be after first.day" =
-             lubridate::yday(last.day) > lubridate::yday(first.day) )
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
 
   list(first.day, last.day)
 }
 
 
 fiat_years = function(first.day, last.day) {
-<<<<<<< HEAD
   dplyr::`%>%`
 
   years = lubridate::year(c(first.day, last.day)) %>%
@@ -178,51 +104,6 @@ import_rdata = function(file) {
 
 
 inmet.args = list(
-=======
-  years = lubridate::year(c(first.day, last.day)) %>%
-    {seq(.[1], .[2])}
-
-  y = list()
-  if (length(years) == 1) {
-    y[[ as.character(years) ]] = list(
-      first.day = first.day,
-      last.day  = last.day
-    )
-  } else {
-    for (i in 1:length(years)) {
-      if (i == 1) {
-        y[[ as.character(years[i]) ]] = list(
-          first.day = first.day,
-          last.day  = paste0(years[1], "/31/12")
-        )
-      } else if (i == length(years)) {
-        y[[ as.character(years[i]) ]] = list(
-          first.day = paste0(years[i], "/01/01"),
-          last.day  = last.day
-        )
-      } else {
-        y[[ as.character(years[i]) ]] = list(
-          first.day = paste0(years[i], "/01/01"),
-          last.day  = paste0(years[i], "/31/12")
-        )
-      }
-    }
-  }
-
-  y
-}
-
-
-verify_hash = function(file) {
-  hashs = list("2004.fst" = "18546dd931bacbe338f09b4121eccf21")
-
-  hashs[[file]]
-}
-
-inmet.args = list(
-  "date"                 = "Data",
-  "hour"                 = "Hora UTC",
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
   "precipitation"        = "PRECIPITAÇÃO TOTAL, HORÁRIO (mm)",
   "atm_pressure"         = "PRESSAO ATMOSFERICA AO NIVEL DA ESTACAO, HORARIA (mB)",
   "atm_pressure_max"     = "PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)",
@@ -241,31 +122,3 @@ inmet.args = list(
   "wind_burst_max"       = "VENTO, RAJADA MAXIMA (m/s)",
   "wind_burst"           = "VENTO, VELOCIDADE HORARIA (m/s)"
 )
-<<<<<<< HEAD
-=======
-
-
-inmet.args2 = list(
-  "Data"                                                  = "date",
-  "Hora UTC"                                              = "hour",
-  "PRECIPITAÇÃO TOTAL, HORÁRIO (mm)"                      = "precipitation",
-  "PRESSAO ATMOSFERICA AO NIVEL DA ESTACAO, HORARIA (mB)" = "atm_pressure",
-  "PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)"       = "atm_pressure_max",
-  "PRESSÃO ATMOSFERICA MIN. NA HORA ANT. (AUT) (mB)"      = "atm_pressure_min",
-  "RADIACAO GLOBAL (Kj/m²)"                               = "radiation",
-  "TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)"          = "temperature_air",
-  "TEMPERATURA DO PONTO DE ORVALHO (°C)"                  = "temperature_dew",
-  "TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"            = "temperature_max",
-  "TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)"            = "temperature_min",
-  "TEMPERATURA ORVALHO MAX. NA HORA ANT. (AUT) (°C)"      = "temperature_dew_max",
-  "TEMPERATURA ORVALHO MIN. NA HORA ANT. (AUT) (°C)"      = "temperature_dew_min",
-  "UMIDADE REL. MAX. NA HORA ANT. (AUT) (%)"              = "humidity_max",
-  "UMIDADE REL. MIN. NA HORA ANT. (AUT) (%)"              = "humidity_min",
-  "UMIDADE RELATIVA DO AR, HORARIA (%)"                   = "humidity",
-  "VENTO, DIREÇÃO HORARIA (gr) (° (gr))"                  = "wind_direction",
-  "VENTO, RAJADA MAXIMA (m/s)"                            = "wind_burst_max",
-  "VENTO, VELOCIDADE HORARIA (m/s)"                       = "wind_burst"
-)
-
-
->>>>>>> 501d05d60fecca2ed93bee86c6bed3afcf686da0
